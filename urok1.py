8923b5c2800fff3493b8a753a7509608b0ad401c
b2114 <dimacuk114@gmail.com>
2025-03-30 11:32:05 +0300

    видання паспорта name age

---
+++ urok1.py
@@ -0,0 +1,13 @@
+'''print("toN")
+print("toN"+"lox")
+print(6*3)
+print("toN"* 10)
+print("мені ", 27, "рочкі")'''
+
+name = input("ведіть своє імя:")
+print("привіт ",name)
+age = int(input("скільки вам років:"))
+if age>=14:
+    print("видати паспорт")
+if age <= 14:
+        print("гуляй")
