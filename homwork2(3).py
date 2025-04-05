ad4a8b0b92b3ce9cb45fb95d8ba89b8cf8c52e61
User <dimacuk114@gmail.com>
2025-04-05 18:04:44 +0300

    це домашка 2 там де 8завдань

---
+++ homwork2(3).py
@@ -0,0 +1,28 @@
+import random
+
+
+zagadane_chyslo = random.randint(1, 10)
+
+print("Я загадав число від 1 до 10. Спробуй вгадати!")
+sprob = 3
+
+while sprob > 0:
+    try:
+        vvid = int(input("Введи своє число: "))
+    except ValueError:
+        print("Будь ласка, введи число!")
+        continue
+
+    if vvid == zagadane_chyslo:
+        print("Вітаю! Ти вгадав число!")
+        break
+    elif vvid > zagadane_chyslo:
+        print("Менше")
+    else:
+        print("Більше")
+
+    sprob -= 1
+    if sprob > 0:
+        print(f"У тебе залишилось спроб: {sprob}")
+    else:
+        print(f"На жаль, ти не вгадав. Я загадав число {zagadane_chyslo}.")
