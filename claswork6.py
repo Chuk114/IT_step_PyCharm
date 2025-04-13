e120927438e8213b2c38b3faa50bf3daa04922e8
b2114 <dimacuk114@gmail.com>
2025-04-13 11:17:11 +0300

    students

---
+++ claswork6.py
@@ -0,0 +1,27 @@
+class Student:
+    number = 0
+
+    def __init__(self, height = 164, name = None):
+        self.height = height
+        self.name = name
+        Student.number = Student.number + 1
+
+    def grow(self, height = 3):
+        self.height = self.height + height
+
+    def __str__(self):
+        return f"Я студент, з іменем {self.name} Мій зріст = {self.height}"
+
+Artem = Student(height = 170, name="Artem")
+Mark = Student(name="Mark")
+
+print(Artem)
+print(Mark)
+
+while Mark.height < 180:
+    Mark.grow()
+    print("My height = ", Artem.height)
+    print("My height = ", Mark.height)
+
+print("number of students = ", Student.number)
+
