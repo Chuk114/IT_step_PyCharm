7b64f822c7978f4cc0febd9f547559129e898473
b2114 <dimacuk114@gmail.com>
2025-04-27 11:02:17 +0300

    The sims

--- The Sims.py
+++ The Sims.py
@@ -7,22 +7,22 @@ class Car:
         self.brand = brand
         self.passengers = []
 
-    def add_passengers(self, human):
-        self.passengers.append(human)
-    def print_passengers(self):
-        if self.passengers:  # Без крапок після if
-            print(f"Names of {self.brand} passengers:")
-            for i in self.passengers:
-                print(i.name)
-        else:
-            print(f"{self.brand} has no passengers.")
+    def add_passengers(self, *args):
+        self.passengers.extend(args)
 
+    def print_passengers(self):
+        if self.passengers:
+            names = ", ".join(i.name for i in self.passengers)
+            print(f"Names of {self.brand} passengers: {names}")
+        else:
+            print(f"In the car {self.brand} there are no passengers.")
 
 dima = Human("Dmytro")
 ihor = Human("Ihor")
+
 car1 = Car("Mercedes")
 car2 = Car()
-car2.add_passengers(dima)
-car1.add_passengers(ihor)
-car1.print_passengers()
+
 car2.print_passengers()
+car1.add_passengers(ihor, dima)
+car1.print_passengers()
